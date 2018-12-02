ee730adbb883f03783fac7ef1353c5aaf6c3d207
Davide Pedron <davide.pedron@thread.solutions>
2018-12-02 20:52:03 +0100

    Rename class

--- src/main/java/Day1part1.java
+++ src/main/java/Day1part1.java
@@ -5,11 +5,11 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
-public class Day1 {
+public class Day1part1 {
 
     public static void main(String[] args) throws IOException, URISyntaxException {
 
-        final URL input = Day1.class.getClassLoader().getResource("day1/input");
+        final URL input = Day1part1.class.getClassLoader().getResource("day1/input");
         Stream<String> lines = (input != null) ? Files.lines(Paths.get(input.toURI())) : Stream.empty();
 
         lines.map(Long::valueOf)
